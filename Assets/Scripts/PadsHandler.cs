afd291562077ec6f0fc7bd383b132298ceb70cb2
Yu Kusanagi <mujinaproduction@gmail.com>
2019-09-24 00:56:48 +0900

    added improvements

--- Assets/Scripts/PadsHandler.cs
+++ Assets/Scripts/PadsHandler.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using AbletonPush2;
+using UnityEngine.Experimental.VFX;
 
 public class PadsHandler : MonoBehaviour
 {
+    public VisualEffect kickReaction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // kickReaction = GetComponent<VisualEffect>();
     }
 
     // Update is called once per frame
@@ -27,11 +29,13 @@ public class PadsHandler : MonoBehaviour
     public void PadPressed(Pad pad, float velocity)
     {
         Push2.SetLED(pad, LED.Color.RGB.Red, LED.Animation.None);
+        kickReaction.SetFloat("level", 1.0f);
     }
 
     public void PadReleased(Pad pad)
     {
         Push2.SetLED(pad, LED.Color.RGB.LightGray);
+        kickReaction.SetFloat("level", 0.0f);
     }
 
     public void AfterTouch(float pressure)
