2c96bfc2a4694aefc0aaadf8249d83df74fb85e9
Yu Kusanagi <mujinaproduction@gmail.com>
2019-09-24 00:16:41 +0900

    added pad handler

---
+++ Assets/Scripts/PadsHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AbletonPush2;
+
+public class PadsHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        Push2.padPressedDelegate += PadPressed;
+        Push2.padReleasedDelegate += PadReleased;
+        Push2.afterTouchDelegate += AfterTouch;
+    }
+
+    public void PadPressed(Pad pad, float velocity)
+    {
+        Push2.SetLED(pad, LED.Color.RGB.Red, LED.Animation.None);
+    }
+
+    public void PadReleased(Pad pad)
+    {
+        Push2.SetLED(pad, LED.Color.RGB.LightGray);
+    }
+
+    public void AfterTouch(float pressure)
+    {
+    }
+}
